214c04251180a7dcba1f54ca5a3a5329eb1225a6
ahmed firoum <ahmedfiroumk@gmail.com>
2025-04-22 17:51:14 +0200

    page/home work in progress

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,25 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="testhome">
+      <main className="main-container-home">
+        <h1>Prépare ton sac, on t'emmène en voyage !</h1>
+        <div className="testnumber2">
+          <p>
+            Découvrez 20 pays fascinants et plongez dans 5 villes emblématiques
+            par destination. <br />
+            <br />
+            Laissez-vous guider vers des aventures inoubliables. Votre escapade
+            commence ici !
+          </p>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            Explorer
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
