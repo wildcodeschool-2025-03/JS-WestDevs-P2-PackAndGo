a0f4340e81275215f696e3b85c9833e012dd59f0
Leïla <leila-dahmani@live.fr>
2025-04-23 16:34:03 +0200

    work in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import Countries from "./components/Countries/Countries";
 function App() {
-  return <h1>Coucou</h1>;
-}
+  return (
+    <>
+      <h1>Coucou</h1>
 
+      <Countries />
+    </>
+  );
+}
 export default App;
---
+++ client/src/components/Countries/Countries.tsx
@@ -0,0 +1,20 @@
+import "./Countries.css";
+
+function Countries() {
+  return (
+    <main>
+      <h1>Choisissez votre future destination</h1>
+      <div>
+        <article>
+          <img
+            src="https://cdn.pixabay.com/photo/2020/11/30/17/43/salt-5791696_1280.jpg"
+            alt="Femme sénégalaise récoltant du sel"
+          />
+          <h2>Sénégal</h2>
+        </article>
+      </div>
+    </main>
+  );
+}
+
+export default Countries;
