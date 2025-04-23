a3a00a6f92609c14f264108ac588f39f7380d4a2
Leïla <leila-dahmani@live.fr>
2025-04-23 17:11:35 +0200

    Biome rules resolved

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,8 +3,12 @@ function Footer() {
   return (
     <div className="footer-container">
       <article>
-      <p>The Adventurers of the lost code</p>
-      <img className="github-logo-left" src="./logo-github.svg" alt="GitHub logo" />
+        <p>The Adventurers of the lost code</p>
+        <img
+          className="github-logo-left"
+          src="./logo-github.svg"
+          alt="GitHub logo"
+        />
       </article>
       <ul>
         <li>
@@ -13,14 +17,18 @@ function Footer() {
         <li>
           <a href="/">CGU</a>
         </li>
-        
+
         <li>
           <a
             href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P2-PackAndGo"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img className="github-logo-right" src="./logo-github.svg" alt="GitHub logo" />
+            <img
+              className="github-logo-right"
+              src="./logo-github.svg"
+              alt="GitHub logo"
+            />
           </a>
         </li>
       </ul>
