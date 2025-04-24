eab579c2fc26cef993c93f170db44a8a7e3a8d6f
Leïla <leila-dahmani@live.fr>
2025-04-24 15:39:26 +0200

    work in progress

--- client/src/pages/Countries/Countries.tsx
+++ client/src/pages/Countries/Countries.tsx
@@ -4,15 +4,17 @@ function Countries() {
   return (
     <main>
       <h1>Choisissez votre future destination</h1>
-      <div>
-        <article>
-          <img
-            src="https://cdn.pixabay.com/photo/2020/11/30/17/43/salt-5791696_1280.jpg"
-            alt="Femme sénégalaise récoltant du sel"
-          />
-          <h2>Sénégal</h2>
-        </article>
-      </div>
+      <figure>
+        <figcaption>Sénégal</figcaption>
+        <img
+          src="https://cdn.pixabay.com/photo/2020/11/30/17/43/salt-5791696_1280.jpg"
+          alt="Femme sénégalaise récoltant du sel"
+        />
+        <p>
+          Découvrez le Sénégal, la terre de la Teranga, où l’hospitalité et les
+          paysages vibrants séduisent les âmes en quête de sérénité.
+        </p>
+      </figure>
     </main>
   );
 }
