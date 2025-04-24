0e6b770e0dbd363db766156d19294166d87020c4
ahmed firoum <ahmedfiroumk@gmail.com>
2025-04-24 16:32:01 +0200

    work in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
