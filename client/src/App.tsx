7ab25a0dab0f534d84f761f6c830196c5b031020
Leïla <leila-dahmani@live.fr>
2025-04-25 12:37:12 +0200

    took off countries import on App.tsx and branch rooter incoming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
-import Countries from "./pages/Countries/Countries";
 
 function App() {
   return (
     <>
       <Header />
-      <Countries />
       <Outlet />
     </>
   );
