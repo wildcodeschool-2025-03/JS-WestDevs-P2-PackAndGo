6830c6028789d228d553d22d94cd99988c62f880
ahmed firoum <ahmedfiroumk@gmail.com>
2025-04-25 11:47:55 +0200

    fixbug/outlet-page-for-footer changed position fixed on pages, add to change some code to see our footer and not destroying our pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
-import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
     </>
   );
 }
