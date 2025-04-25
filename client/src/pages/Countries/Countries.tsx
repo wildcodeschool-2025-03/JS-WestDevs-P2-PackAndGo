9fef01419d76c61d48ed059142d6fdc8f6424013
Leïla <leila-dahmani@live.fr>
2025-04-25 12:14:56 +0200

    Added a state for my object map

--- client/src/pages/Countries/Countries.tsx
+++ client/src/pages/Countries/Countries.tsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
 import "./Countries.css";
 
 function Countries() {
+  const [countriesList] = useState([
+    {
+      id: 1,
+      name: "France",
+      image:
+        "https://cdn.pixabay.com/photo/2018/04/25/09/26/eiffel-tower-3349075_1280.jpg",
+      tagline:
+        "Laissez-vous captiver par la richesse culturelle et la beauté intemporelle de la France, un mélange de romantisme et de gastronomie.",
+    },
+    {
+      id: 2,
+      name: "Sénégal",
+      image:
+        "https://cdn.pixabay.com/photo/2020/11/30/17/43/salt-5791696_1280.jpg",
+      tagline:
+        "Découvrez le Sénégal, la terre de la Teranga, où l’hospitalité et les paysages vibrants séduisent les âmes en quête de sérénité.",
+    },
+  ]);
   return (
-    <main>
-      <h1>Choisissez votre future destination</h1>
-      <figure>
-        <figcaption>Sénégal</figcaption>
-        <img
-          src="https://cdn.pixabay.com/photo/2020/11/30/17/43/salt-5791696_1280.jpg"
-          alt="Femme sénégalaise récoltant du sel"
-        />
-        <p>
-          Découvrez le Sénégal, la terre de la Teranga, où l’hospitalité et les
-          paysages vibrants séduisent les âmes en quête de sérénité.
-        </p>
-        <p>test</p>
-      </figure>
+    <main className="countries-main-container">
+      <h1>Sélectionnez vots futures destinations</h1>
+      {countriesList.map((element) => (
+        <div key={element.id} className="stack">
+          <figure className="card">
+            <figcaption>{element.name}</figcaption>
+            <span className="love">
+              <input id="switch" type="checkbox" />
+              <label className="love-heart" htmlFor="switch">
+                <i className="left" />
+                <i className="right" />
+                <i className="bottom" />
+                <div className="round" />
+              </label>
+            </span>
+            <img
+              className="image"
+              src={element.image}
+              alt="Femme sénégalaise récoltant du sel"
+            />
+            <p>{element.tagline}</p>
+          </figure>
+        </div>
+      ))}
     </main>
   );
 }
