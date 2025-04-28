62bf9c45d1be0d4258b15656ce163de6d9c35e55
ahmed firoum <ahmedfiroumk@gmail.com>
2025-04-28 12:35:51 +0200

    work in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router";
+// import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Town from "./pages/Town/Town";
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      {/* <Outlet /> */}
+      <Town />
       <Footer />
     </>
   );
--- client/src/pages/Town/Town.tsx
+++ client/src/pages/Town/Town.tsx
@@ -1,5 +1,52 @@
 import "./Town.css";
+// import { useEffect, useState } from "react";
+
 function Town() {
-  return <h1>Coucou</h1>;
+  return <h1>Commit</h1>;
+  // const [city, setCity] = useState([]);
+  // const [belge, setBelge] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:3310/api/cities")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log("Données reçues:", data);
+  //       setCity(data);
+  //       const filteredBelgium = data.filter((city) =>
+  //         city.data.attributes.name.toLowerCase().includes("belgium"),
+  //       );
+  //       setBelge(filteredBelgium);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Erreur détaillée:", error);
+  //     });
+  // }, []);
+  // return (
+  //   <main>
+  //     <h1>Belgium</h1>
+  //     <div className="list-cities">
+  //       {/* {belge.map((city) => (
+  //         <div key={city.data.id} className="city-item">
+  //           <h2>{city.data.attributes.short_name}</h2>
+  //           <img
+  //             src={
+  //               city.included.find((item) => item.type === "photo").attributes
+  //                 .image.medium
+  //             } //Même image à chaque fois... Dégoûté...
+  //             alt="test"
+  //           />
+  //         </div>
+  //       ))} */}
+  //       {belge.map((city) => (
+  //         <div key={city.data.id}: any className="city-item">
+  //           <h2>{city.data.attributes.short_name}</h2>
+  //         </div>
+  //       ))}
+  //     </div>
+  //     {/* Test nombre de ville, censé être 5 sinon, il y a une erreur. */}
+  //     <p>{belge.length}</p>
+  //   </main>
+  // );
 }
 export default Town;
