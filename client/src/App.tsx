53f5a44f373ff584dfbafdb749b10119aaefdc88
ahmed firoum <ahmedfiroumk@gmail.com>
2025-04-29 10:49:14 +0200

    chore/page-town-fetch-api fetch api done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Town from "./pages/Town/Town";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <Town />
       <Footer />
     </>
   );
