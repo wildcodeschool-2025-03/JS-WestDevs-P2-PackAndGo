e972732b5170944dcf97f978f8583756a5f11e2d
Leïla <leila-dahmani@live.fr>
2025-04-29 16:28:03 +0200

    fetch my datastorage into my countries page + changed the toggle because I coudn't make it smaller + added description on each countries picture

--- client/src/pages/Countries/Countries.tsx
+++ client/src/pages/Countries/Countries.tsx
@@ -6,10 +6,12 @@ interface countryProp {
   name: string;
   image: string;
   tagline: string;
+  description: string;
 }
 
 function Countries() {
   const [countriesList, setCountriesList] = useState<countryProp[]>([]);
+  const [favorites, setFavorites] = useState<Record<number, boolean>>({});
 
   useEffect(() => {
     fetch("http://localhost:3310/countries")
@@ -17,10 +19,17 @@ function Countries() {
       .then((data) => setCountriesList(data.results));
   }, []);
 
+  const toggleFavorite = (id: number) => {
+    setFavorites((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <main className="countries-main-container">
+      <h1>Sélectionnez vos futures destinations</h1>
       <section>
-        <h1>Sélectionnez vos futures destinations</h1>
         <div className="section-banner">
           <div id="star-1">
             <div className="curved-corner-star">
@@ -32,7 +41,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-2">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -43,7 +51,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-3">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -54,7 +61,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-4">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -65,7 +71,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-5">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -76,7 +81,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-6">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -87,7 +91,6 @@ function Countries() {
               <div id="curved-corner-topleft" />
             </div>
           </div>
-
           <div id="star-7">
             <div className="curved-corner-star">
               <div id="curved-corner-bottomright" />
@@ -100,20 +103,44 @@ function Countries() {
           </div>
         </div>
       </section>
+
       {countriesList.map((element) => (
         <div key={element.id} className="stack">
           <figure className="card">
-            <figcaption>{element.name}</figcaption>
-            <span className="love">
-              <input id={`switch-${element.id}`} type="checkbox" />
-              <label className="love-heart" htmlFor={`switch-${element.id}`}>
-                <i className="left" />
-                <i className="right" />
-                <i className="bottom" />
-                <div className="round" />
+            <figcaption>
+              {element.name}
+              <label htmlFor={`favorite-${element.id}`} className="container">
+                <input
+                  type="checkbox"
+                  id={`favorite-${element.id}`}
+                  name="favorite-checkbox"
+                  value="favorite-button"
+                  checked={favorites[element.id] || false}
+                  onChange={() => toggleFavorite(element.id)}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill={favorites[element.id] ? "hsl(0deg 100% 50%)" : "none"}
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-heart"
+                >
+                  <title>Ajouter aux favoris</title>
+                  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+                </svg>
               </label>
-            </span>
-            <img className="image" src={element.image} alt={element.name} />
+            </figcaption>
+
+            <img
+              className="image"
+              src={element.image}
+              alt={element.description}
+            />
             <p>{element.tagline}</p>
           </figure>
         </div>
