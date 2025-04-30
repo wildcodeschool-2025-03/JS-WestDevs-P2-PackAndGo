7733dfb0c6c4fe519ea459cbc85d889ac1665a6b
Leïla <leila-dahmani@live.fr>
2025-04-30 09:48:33 +0200

    took off the import Countriespage from the App.tsx folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Countries from "./pages/Countries/Countries";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <Countries />
       <Footer />
     </>
   );
