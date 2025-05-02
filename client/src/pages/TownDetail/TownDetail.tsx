c36f24e185f61d2f40bf586f4033c5d481b33774
ahmed firoum <ahmedfiroumk@gmail.com>
2025-05-02 09:46:45 +0200

    feature/town-detail-page pr return

--- client/src/pages/TownDetail/TownDetail.tsx
+++ client/src/pages/TownDetail/TownDetail.tsx
@@ -37,19 +37,16 @@ function TownDetail() {
           <p>Population : {city.data.attributes.population}</p>
         )}
         <article>
-          {" "}
-          <br />
           <h3>For budget hotel: </h3>
           <p>
             {city.data.attributes.info.accommodation.budget_hotel.average_price}{" "}
             {city.data.attributes.info.accommodation.budget_hotel.currency}
           </p>
           <p>
-            {" "}
             {city.data.attributes.info.accommodation.budget_hotel.description}
           </p>
         </article>
-        <br />
+
         <article>
           <h3>For budget hostel classic:</h3>
           <p>
@@ -58,7 +55,7 @@ function TownDetail() {
           </p>
           <p> {city.data.attributes.info.accommodation.hostel.description}</p>
         </article>
-        <br />
+
         <article>
           <h3>For midrange hotel :</h3>
           <p>
@@ -72,7 +69,7 @@ function TownDetail() {
             {city.data.attributes.info.accommodation.midrange_hotel.description}
           </p>
         </article>
-        <br />
+
         <article>
           <h3>For luxury hotel: </h3>
           <p>
@@ -83,42 +80,38 @@ function TownDetail() {
             {city.data.attributes.info.accommodation.luxury_hotel.description}
           </p>
         </article>
-        <br />
         <article>
           <h3>Local food :</h3>
           <p>{city.data.attributes.info.local_food.specialties} </p>
-          <br />
+
           <p> {city.data.attributes.info.local_food.description}</p>
-          <br />
 
           <p> {city.data.attributes.info.local_food.tips}</p>
         </article>
-        <br />
+
         <article>
           <h3>Transport :</h3>
           <p>
             Les différents moyen de transport:{" "}
             {city.data.attributes.info.transport.public.types}
           </p>
-          <br />
+
           <p>
             {city.data.attributes.info.transport.public.average_ticket}{" "}
             {city.data.attributes.info.transport.public.currency}
           </p>
-          <br />
 
           <p> {city.data.attributes.info.transport.public.description}</p>
           <p> {city.data.attributes.info.transport.public.tips}</p>
-          <br />
+
           <h4>Taxi average price</h4>
           <p>
             {city.data.attributes.info.transport.taxi.average_fare}{" "}
             {city.data.attributes.info.transport.taxi.currency}
-            <br />
             <p>{city.data.attributes.info.transport.taxi.description}</p>
             <p>{city.data.attributes.info.transport.taxi.tips}</p>
           </p>
-          <br />
+
           <h4>Taxi average price</h4>
           <p>
             {
@@ -126,25 +119,22 @@ function TownDetail() {
                 .average_price_per_day
             }{" "}
             {city.data.attributes.info.transport.bike_rental.currency}
-            <br />
             <p>{city.data.attributes.info.transport.bike_rental.description}</p>
             <p>{city.data.attributes.info.transport.bike_rental.tips}</p>
           </p>
         </article>
-        <br />
-        <br />
+
         <article>
           <h3>Culture : </h3>
-          <br />
+
           <p>{city.data.attributes.info.culture.must_see}</p>
           <p>{city.data.attributes.info.culture.description}</p>
           <p>{city.data.attributes.info.culture.tips}</p>
         </article>
-        <br />
-        <br />
+
         <article>
           <h3>Budget : </h3>
-          <br />
+
           <p>{city.data.attributes.info.budget.is_expensive}</p>
           <p>
             {city.data.attributes.info.budget.daily_average}
