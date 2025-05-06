1fc3b9d14ec55b021ef5348039a29de0f43f947d
ahmed firoum <ahmedfiroumk@gmail.com>
2025-05-06 10:26:20 +0200

    feature/auth-context done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <AuthProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </AuthProvider>
     </>
   );
 }
