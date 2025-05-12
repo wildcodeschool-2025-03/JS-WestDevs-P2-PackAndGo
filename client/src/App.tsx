759ca79b437c04b72e3636cf58d0dd85f62c5214
Leïla <leila-dahmani@live.fr>
2025-05-12 11:25:06 +0200

    add a function get and set storage for the countries from destination page to the favorites page + a window clear function in case the user close the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { AuthProvider } from "./hooks/AuthContext";
 
 function App() {
-  useEffect(() => {
-    const clearLocalStorageOnExit = () => {
-      localStorage.clear();
-    };
-
-    window.addEventListener("beforeunload", clearLocalStorageOnExit);
-
-    return () => {
-      window.removeEventListener("beforeunload", clearLocalStorageOnExit);
-    };
-  }, []);
   return (
     <>
       <AuthProvider>
