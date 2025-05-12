2a0d13168e217aebf09f33183db49b8745ef0c8a
Leïla <leila-dahmani@live.fr>
2025-05-12 11:11:30 +0200

    add a function get and set storage for the countries from destination page to the favorites page + a window clear function in case the user close the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
